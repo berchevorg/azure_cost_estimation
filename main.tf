bfc8ec2c594f7a5ed3744aa89f085301311882c0
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2020-11-19 15:51:58 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+    features {}
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "api-rg-pro"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "api-appserviceplan-pro"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "PremiumV3"
+    size = "P1v3"
+  }
+}
